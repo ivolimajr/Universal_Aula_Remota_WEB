4fbc31ebcf8e1400232f892a15b58fef8a298f21
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-06-18 11:23:56 -0300

    Remoção dos cards de busca

--- src/app/_metronic/partials/layout/extras/extras.module.ts
+++ src/app/_metronic/partials/layout/extras/extras.module.ts
@@ -5,14 +5,8 @@ import { InlineSVGModule } from 'ng-inline-svg';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-import { SearchDropdownInnerComponent } from './dropdown-inner/search-dropdown-inner/search-dropdown-inner.component';
-import { NotificationsDropdownInnerComponent } from './dropdown-inner/notifications-dropdown-inner/notifications-dropdown-inner.component';
-import { QuickActionsDropdownInnerComponent } from './dropdown-inner/quick-actions-dropdown-inner/quick-actions-dropdown-inner.component';
-import { CartDropdownInnerComponent } from './dropdown-inner/cart-dropdown-inner/cart-dropdown-inner.component';
 import { UserDropdownInnerComponent } from './dropdown-inner/user-dropdown-inner/user-dropdown-inner.component';
-import { SearchResultComponent } from './dropdown-inner/search-dropdown-inner/search-result/search-result.component';
 import { CoreModule } from '../../../core';
-import { ScrollTopComponent } from './scroll-top/scroll-top.component';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
@@ -20,13 +14,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
 
 @NgModule({
   declarations: [
-    SearchDropdownInnerComponent,
-    NotificationsDropdownInnerComponent,
-    QuickActionsDropdownInnerComponent,
-    CartDropdownInnerComponent,
     UserDropdownInnerComponent,
-    SearchResultComponent,
-    ScrollTopComponent,
   ],
   imports: [CommonModule, InlineSVGModule, PerfectScrollbarModule, CoreModule, RouterModule],
   providers: [
@@ -36,12 +24,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     },
   ],
   exports: [
-    SearchDropdownInnerComponent,
-    NotificationsDropdownInnerComponent,
-    QuickActionsDropdownInnerComponent,
-    CartDropdownInnerComponent,
     UserDropdownInnerComponent,
-    ScrollTopComponent,
   ],
 })
 export class ExtrasModule { }
